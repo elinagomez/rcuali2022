6c2d044ccb326d349bd9bf64eb21774a12a3ac0a
elinagomez <45207375+elinagomez@users.noreply.github.com>
2022-11-17 12:58:19 -0300

    clase 3 completa

---
+++ Clase3/Material/live_coding_3.R
@@ -0,0 +1,166 @@
+
+
+#librerías generales
+
+library(dplyr)
+library(ggplot2)
+library(rtweet)
+
+##1. Creamos la App en Twitter Dev
+##2. Copiamos el bearer token
+##3. Corremos la función rtweet_app() asignada a un objeto
+##4. Guardamos con la función auth_save() 
+##5. Cada vez cargamos con auth_as() 
+
+
+##Cargamos las credenciales para conectarnos
+
+auth_as("authRCuali")
+
+#buscamos los tweets que mencionan un determinado usuarix o hashtag 
+
+fa <- search_tweets("@Frente_amplio", n = 18000, include_rts = TRUE,retryonratelimit=TRUE)
+
+
+#retryonratelimit=TRUE
+
+## si quisiera hacer búsquedas conjuntas
+
+#facc <- search_tweets('"frente amplio" OR "carolina cosse"', n = 18000, include_rts = TRUE,retryonratelimit=TRUE)
+
+
+#'"frente amplio"' / OR / AND
+
+
+load("~/rcuali2022/Clase3/Material/fa.RData")
+
+
+##graficamos usando la función ts_plot para series de tiempo en twitter
+
+
+fa %>%
+  ts_plot("5 hours") +
+  ggplot2::theme(plot.title = ggplot2::element_text(face = "bold")) +
+  ggplot2::labs(
+    x = NULL, y = NULL,
+    title = "Frecuencia de @Frente_Amplio de los últimos 9 días",
+    caption = "Recuento de tweets en intervalos de 5 horas"
+  )
+
+
+
+##Utilizamos las funciones get_followers() para obtener lxs usuarixs y 
+#lookup_users para obtener información de lxs mismxs
+
+##Análisis de politicos/as: Carolina Cosse y Yamandu Orsi
+##trae 75000 cada 15 minutos
+
+#@CosseCarolina
+
+cc_flw <- get_followers("CosseCarolina",n = Inf,retryonratelimit = TRUE)
+
+
+cc_flw_data <- lookup_users(cc_flw$from_id) ##trae hasta 90000 seguidores/as
+
+
+#save(cc_flw_data,file="Clase3/Material/CosseCarolina_2021.RData")
+
+#OrsiYamandu
+
+orsi_flw <- get_followers("OrsiYamandu", n = "all",retryonratelimit=TRUE)
+
+orsi_flw_data <- lookup_users(orsi_flw$from_id)
+
+#save(orsi_flw_data,file="Clase3/Material/OrsiYamandu_2021.RData")
+
+
+#genero una base por candidata, agrego una variable con el nombre 
+#y una de conteo antes de unirlas
+
+
+load("~/rcuali2022/Clase3/Material/CosseCarolina_2021.RData")
+load("~/rcuali2022/Clase3/Material/OrsiYamandu_2021.RData")
+
+cc_flw_data <- cc_flw_data %>%
+  mutate(candi="Carolina Cosse",
+         cc_c=1)
+
+orsi_flw_data <- orsi_flw_data %>%
+  mutate(candi="Yamandú Orsi",
+         yam_c=1)
+
+
+#Pongo todos los data.frames de las candidatas en una lista
+bases <- list(cc_flw_data, orsi_flw_data)
+
+#Uno todas las bases y selecciono las variables que me interesan                                    
+seguidores<-bind_rows(bases) %>%
+  select(user_id, text ,name, screen_name,country, country_code,geo_coords,coords_coords,
+         location, followers_count, statuses_count, account_created_at,candi,
+         ends_with('_c'))
+
+#seguidores[is.na(seguidores)] = 0
+
+
+
+##grafico la cantidad de seguidorxs por cada candidata
+
+#save(seguidores,file="Clase3/Material/seguidores_2021.RData")
+
+load("~/rcuali2022/Clase3/Material/seguidores_2021.RData")
+
+library(RColorBrewer)
+require(forcats)
+
+ggplot(seguidores, aes(fct_infreq(candi)))+ geom_bar(fill= c("#FFB5E8","85E3FF")) +
+  geom_text(stat='count', aes(label=..count..), vjust=-1)+
+   xlab("Politicxs") +
+  ylab("Cantidad de seguidorxs")
+
+
+
+## Uso y gráfico de get_timelines
+
+serie_cc <- get_timelines(c("CosseCarolina"), n = 3200)
+serie_cc$screen_name="CosseCarolina"
+serie_lp <- get_timelines(c("LuisLacallePou"), n = 3200)
+serie_lp$screen_name="LuisLacallePou"
+serie=rbind(serie_cc,serie_lp)
+
+#load("~/rcuali2022/Clase3/Material/swriecclp.RData")
+
+serie %>%
+  dplyr::filter(created_at > "2021-07-01") %>%
+  dplyr::group_by(screen_name,created_at) %>%
+  summarize(n=n())%>%
+  group_by(week = cut(created_at, "week"),screen_name) %>% summarise(value = sum(n))%>%
+  ggplot(ggplot2::aes(x = as.Date(week), y = value, group=screen_name,colour=screen_name)) +
+  geom_line()+
+  ggplot2::geom_point() +
+  scale_x_date(date_breaks = "4 month")+
+  ggplot2::theme_minimal() +
+  ggplot2::theme(
+    legend.title = ggplot2::element_blank(),
+    legend.position = "bottom",
+    plot.title = ggplot2::element_text(face = "bold")) +
+  ggplot2::labs(
+    x = NULL, y = NULL,
+    title = "Frecuencia de los estados de Twitter publicados",
+    subtitle = "Recuento de estados de Twitter agregados por semana - Julio 2021/Noviembre 2022"
+  )
+
+
+
+##Ejercicio
+
+##1. Hacer una búsqueda de los tweets con una palabra clave, sin incluir re tweets
+
+##2. Realizar un gráfico con serie de tiempo de los últimos con actualización cada un 12 horas
+
+##3. Extraer lxs seguidorxs de una cuenta específica y graficar alguna de sus características
+
+
+
+
+
+
